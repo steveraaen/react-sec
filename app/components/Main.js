625ab7fd7766e2b503fad1d781c52dc33a3bec6c
steveraaen <sraaen@gmail.com>
2017-06-17 17:04:50 -0400

    basic ui structure

--- app/components/Main.js
+++ app/components/Main.js
@@ -7,7 +7,39 @@ class Main extends React.Component {
 	}
 	render() {
 		return (
-			<div>Hello</div>
+			<div className="container">
+				<div className="header">
+				Header
+				</div>
+					<div className="container">
+
+					
+						<div className="row">
+							<div className="col-md-4">	
+								<h3>col-md-4</h3>
+								<div className="well">
+									<table>
+										<tr>001</tr>
+										<tr>002</tr>
+										<tr>003</tr>
+										<tr>004</tr>
+									</table>
+									</div>
+									<br/>
+									<div className="well">
+									<h1>Well</h1>
+									</div>					
+							</div>
+							<div className="col-md-8">
+								<h3>col-md-8</h3>					
+							</div>
+						</div>
+					</div>
+				<div className="footer">
+				Footer
+				</div>
+
+			</div>
 			)
 	}
 }
